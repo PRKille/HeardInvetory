6ed8cc667896445c9d474e6117a33b00554667ab
Patrick Kille <pr.kille@gmail.com>
2020-05-08 16:47:01 -0700

    migrate and update database

---
+++ HeardInventory/Migrations/20200508234529_AddCategoryAndVendor.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace HeardInventory.Migrations
+{
+    public partial class AddCategoryAndVendor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "Vendor",
+                table: "Items",
+                newName: "ItemName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "ItemId");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CategoryName = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    VendorId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    VendorName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.VendorId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CategoryId",
+                table: "Items",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_VendorId",
+                table: "Items",
+                column: "VendorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Vendors_VendorId",
+                table: "Items",
+                column: "VendorId",
+                principalTable: "Vendors",
+                principalColumn: "VendorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Vendors_VendorId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Vendors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_VendorId",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemName",
+                table: "Item",
+                newName: "Vendor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Item",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Item",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "ItemId");
+        }
+    }
+}
