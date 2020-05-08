df2f0536f956c36946800cbaf248ed71197ed60c
Patrick Kille <pr.kille@gmail.com>
2020-05-08 13:33:39 -0700

    install correct dependencies

--- HeardInventory/Startup.cs
+++ HeardInventory/Startup.cs
@@ -43,7 +43,7 @@ namespace HeardInventory
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
