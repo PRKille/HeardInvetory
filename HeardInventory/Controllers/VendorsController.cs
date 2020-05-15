bc9c3c6e541894c59960a53f7d8432431ae14c14
Patrick Kille <pr.kille@gmail.com>
2020-05-15 10:44:18 -0700

    create vendor controller

--- HeardInventory/Controllers/VendorsController.cs
+++ HeardInventory/Controllers/VendorsController.cs
@@ -1,7 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-using HeardInventory.Models;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+using HeardInventory.Models;
 
 namespace HeardInventory.Controllers
 {
@@ -14,10 +16,40 @@ namespace HeardInventory.Controllers
       _db = db;
     }
 
-    public ActionResult Index()
+    [HttpGet]
+    public ActionResult <IEnumerable<Vendor>> Get()
     {
-      List<Vendor> model = _db.Vendors.ToList();
-      return View(model);
+      return _db.Vendors.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Vendor> Get(int id)
+    {
+      return _db.Vendors.FirstOrDefault(cat => cat.VendorId == id);
+    }
+
+    [EnableCors("MyPolicy")]
+    [HttpPost]
+    public void Post([FromBody] Vendor Vendor)
+    {
+      _db.Vendors.Add(Vendor);
+      _db.SaveChanges();
+    }
+
+    [HttpPut]
+    public void Put(int id, [FromBody] Vendor Vendor)
+    {
+      Vendor.VendorId = id;
+      _db.Entry(Vendor).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      Vendor vendorForDeletion = _db.Vendors.FirstOrDefault(vendor => vendor.VendorId == id);
+      _db.Vendors.Remove(vendorForDeletion);
+      _db.SaveChanges();
     }
   }
 }
