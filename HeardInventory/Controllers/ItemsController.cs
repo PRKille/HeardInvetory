8e345f88cfeff6ca6f40758d755404a100c10d22
Patrick Kille <pr.kille@gmail.com>
2020-05-15 09:28:31 -0700

    finish basic item controller routes

--- HeardInventory/Controllers/ItemsController.cs
+++ HeardInventory/Controllers/ItemsController.cs
@@ -1,7 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-using HeardInventory.Models;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+using HeardInventory.Models;
 
 namespace HeardInventory.Controllers
 {
@@ -25,7 +27,45 @@ namespace HeardInventory.Controllers
       {
         query = query.Where(item => item.ItemName == name);
       }
+      if(category != null)
+      {
+        query = query.Where(item => item.Category.CategoryName == category);
+      }
+      if(vendor != null)
+      {
+        query = query.Where(item => item.Vendor.VendorName == vendor);
+      }
       return query.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Item> Get(int id)
+    {
+      return _db.Items.FirstOrDefault(item => item.ItemId == id);
+    }
+
+    [EnableCors("MyPolicy")]
+    [HttpPost]
+    public void Post([FromBody] Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+    }
+
+    [HttpPut]
+    public void Put(int id, [FromBody] Item item)
+    {
+      item.ItemId = id;
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      Item itemForDeletion = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(itemForDeletion);
+      _db.SaveChanges();
     }
   }
 }
