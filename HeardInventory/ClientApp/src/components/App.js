49f3e03efe5b477b27f957e2506898b6f1691807
Patrick Kille <pr.kille@gmail.com>
2020-05-15 16:53:45 -0700

    set up component files for react front end

---
+++ HeardInventory/ClientApp/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <h1>Heard! Inventory Solutions</h1>
+    );
+  }
+}
