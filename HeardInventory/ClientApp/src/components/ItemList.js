2b312104607168a157b5cee8d0ad6e194d1cc800
Patrick Kille <pr.kille@gmail.com>
2020-05-18 13:31:04 -0700

    added fetch calls to populate category and vendor state, utilized state to display category and vendor names

--- HeardInventory/ClientApp/src/components/ItemList.js
+++ HeardInventory/ClientApp/src/components/ItemList.js
@@ -3,10 +3,32 @@ import Item from './Item';
 
 function ItemList() {
   const [ itemListState, setItemListState ] = useState([]);
+  const [ categoryListState, setCategoryListState ] = useState([]);
+  const [ vendorListState, setVendorListState ] = useState([]);
   const [ loadState, setLoadState ] = useState(false);
 
   useEffect(() => {
     if (!loadState) {
+      fetch(`http://localhost:5000/api/categories`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonifiedResponse) => {
+        setCategoryListState(jsonifiedResponse);
+      })
+      .catch((error) => {
+        console.log('Category Load Error: ', error);
+      });
+      fetch(`http://localhost:5000/api/vendors`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonifiedResponse) => {
+        setVendorListState(jsonifiedResponse);
+      })
+      .catch((error) => {
+        console.log('Venor Load Error: ', error);
+      });
       fetch(`http://localhost:5000/api/items`)
       .then((response) => {
         return response.json();
@@ -26,11 +48,13 @@ function ItemList() {
       <React.Fragment>
         <h1>Inventory Items</h1>
         {itemListState.map((item) => {
+          var categoryName = categoryListState[item.categoryId-1].categoryName;
+          var vendorName = vendorListState[item.vendorId-1].vendorName;
           return (
             <Item
               itemName={item.itemName}
-              category={item.category}
-              vendor={item.vendor}
+              category={categoryName}
+              vendor={vendorName}
               purchasePrice={item.purchasePrice}
               purchaseQuantity ={item.purchaseQuantity}
               purchaseQuantityType={item.purchaseQuantityType}
