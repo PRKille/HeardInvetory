7bbb77050272186e9ad9ab4baafa39d15c33dd8e
Patrick Kille <pr.kille@gmail.com>
2020-05-18 09:36:52 -0700

    create Item object

---
+++ HeardInventory/ClientApp/src/components/Item.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Item(props) {
+  return (
+    <React.Fragment>
+      <h1>{props.itemName}</h1>
+      <ul>
+        <li>Category: {props.category}</li>
+        <li>Vendor: {props.vendor}</li>
+        <li>Purchase Info: ${props.purchasePrice}/{props.purchaseQuantity}{props.purchaseQuantityType}</li>
+        <li>Last Inventory: {props.startingInventory} {props.purchaseQuantityType}</li>
+      </ul>
+    </React.Fragment>
+  )
+}
+
+Item.propTypes = {
+  itemName: PropTypes.string,
+  category: PropTypes.string,
+  vendor: PropTypes.string,
+  purchasePrice: PropTypes.number,
+  purchaseQuantity: PropTypes.number,
+  purchaseQuantityType: PropTypes.string,
+  startingInventory: PropTypes.number
+}
