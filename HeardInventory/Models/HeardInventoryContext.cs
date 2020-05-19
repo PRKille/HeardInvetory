55213bdc306bd30e974838973b7bc0aab867ce4c
Patrick Kille <pr.kille@gmail.com>
2020-05-19 16:32:47 -0700

    add in audits to context

--- HeardInventory/Models/HeardInventoryContext.cs
+++ HeardInventory/Models/HeardInventoryContext.cs
@@ -10,6 +10,7 @@ namespace HeardInventory.Models
     public DbSet<Item> Items {get;set;}
     public DbSet<Category> Categories {get;set;}
     public DbSet<Vendor> Vendors {get;set;}
+    public DbSet<Audit> Audits {get;set;}
     public HeardInventoryContext(DbContextOptions<HeardInventoryContext> options) : base(options)
     {
 
