f52bbc6094ffd5e1dc7119ba55c692c272cb683a
Patrick Kille <pr.kille@gmail.com>
2020-05-19 10:21:48 -0700

    update database to remove lazy loading

--- HeardInventory/Migrations/20200519164940_RemoveLazyLoadingAttributesFromCatAndVen.Designer.cs
+++ HeardInventory/Migrations/20200519164940_RemoveLazyLoadingAttributesFromCatAndVen.Designer.cs
@@ -2,15 +2,17 @@
 using HeardInventory.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HeardInventory.Migrations
 {
     [DbContext(typeof(HeardInventoryContext))]
-    partial class HeardInventoryContextModelSnapshot : ModelSnapshot
+    [Migration("20200519164940_RemoveLazyLoadingAttributesFromCatAndVen")]
+    partial class RemoveLazyLoadingAttributesFromCatAndVen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HeardInventory/Migrations/20200519164940_RemoveLazyLoadingAttributesFromCatAndVen.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HeardInventory.Migrations
+{
+    public partial class RemoveLazyLoadingAttributesFromCatAndVen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
